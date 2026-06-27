b286f74504d8488b100188a4d67dc1bf19b98080
Umar71-Developer <umarhayyat710000@gmail.com>
2026-06-27 12:31:02 +0500

    Star Rating Project is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Accordian from './components/accordian'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
   return <div className='app'>
     {/* Accordian Component */}
     {/* <Accordian /> */}
     {/* Random Color Generator Component */}
-    <RandomColor />
+    {/* <RandomColor /> */}
+    {/* Star Rating Component */}
+    <StarRating />
     </div>
   
 }
