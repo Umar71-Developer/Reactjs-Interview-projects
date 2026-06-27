e03e18eee68a6c33cf875661d26673042a371700
Umar71-Developer <umarhayyat710000@gmail.com>
2026-06-27 15:22:09 +0500

    imag-slider component is completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Accordian from './components/accordian'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
+import ImageSlider from './components/image-slider'
 
 function App() {
   return <div className='app'>
@@ -10,9 +11,12 @@ function App() {
     {/* Random Color Generator Component */}
     {/* <RandomColor /> */}
     {/* Star Rating Component */}
-    <StarRating />
-    </div>
-  
+    {/* <StarRating /> */}
+
+    {/* Image Slider Component */}
+    <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"10"} />
+  </div>
+
 }
 
 export default App
